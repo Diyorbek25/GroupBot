d1f4d24cbb14214e0cb0237b2c2b6a9a62cc2056
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-03-03 22:20:53 +0500

    Add project files.

---
+++ GroupBot.Core/Controllers/BotController.cs
@@ -0,0 +1,28 @@
+﻿using GroupBot.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupBot.Core.Controllers;
+
+[ApiController]
+[Route("bot")]
+public class BotController : ControllerBase
+{
+    private BotService botService;
+
+    public BotController(BotService botService)
+    {
+        this.botService = botService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(
+        long groupId,
+        string message)
+    {
+
+        await botService
+            .SendMessageToGroupAsync(groupId, message);
+
+        return Ok();
+    }
+}
---
+++ GroupBot.Core/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using GroupBot.Core.Services;
+using Telegram.Bot;
+
+namespace GroupBot.Core.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddTelegramBot(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string botToken = configuration
+            .GetSection("TelegramBot:Token").Value;
+
+        services.AddSingleton<ITelegramBotClient, TelegramBotClient>(
+            botClient => new TelegramBotClient(botToken));
+
+        return services;
+    }
+
+    public static IServiceCollection AddTelegramBotService(
+        this IServiceCollection services)
+    {
+        services.AddScoped<BotService>();
+        return services;
+    }
+}
